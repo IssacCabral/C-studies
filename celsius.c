9411669bb7cb165df8ffd9bf035459758338aac6
Clidenor Issac de Almeida Cabral <clidenorissac@gmail.com>
2026-07-22 15:18:36 -0300

    use Symbolic Constants

--- celsius.c
+++ celsius.c
@@ -1,18 +1,26 @@
 #include <stdio.h>
 
-int main() {
+#define LOWER 0     /* lower limit of table */
+#define UPPER 300   /* upper limit */
+#define STEP  20    /* step size */
+
+void printHeader() {
+  printf("%10s %10s\n", "Fahrenheit", "Celsius");
+  printf("%10s %10s\n", "----------", "-------");
+}
+
+void printConversion() {
   float fahr, celsius;
   float lower, upper, step;
 
-  lower = 0;
-  upper = 300;
-  step = 20;
-
-  fahr = lower;
-  while(fahr <= upper) {
+  for (fahr = LOWER; fahr <= UPPER; fahr += STEP) {
     celsius = (5.0 / 9.0) * (fahr - 32.0);
-    printf("%3.0f %6.1f\n", fahr, celsius);
-    fahr = fahr + step;
+    printf("%10.0f %10.1f\n", fahr, celsius);
   }
+}
+
+int main() {
+  printHeader();
+  printConversion();
   return 0;
 }
